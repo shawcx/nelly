09b884dd726ecb498c67082e0393442871f5fcde
Matthew Oertle <moertle@gmail.com>
2019-01-10 16:49:32 -0500

    v0.6.0

--- nelly/version.py
+++ nelly/version.py
@@ -1,7 +1,7 @@
 
 __author__    = 'Matthew Oertle'
 __email__     = 'moertle@gmail.com'
-__copyright__ = '(c)2008-2018, Matthew Oertle'
+__copyright__ = '(c)2008-2019, Matthew Oertle'
 __credits__   = ['Matthew Oertle']
 __license__   = 'MIT'
-__version__   = '0.5.0'
+__version__   = '0.6.0'
