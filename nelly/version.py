7bfcfd297e4d6b2d5a6849f61d0ce299ccd7b169
Matthew Shaw <moertle@cromulence.com>
2020-11-10 22:09:50 -0500

    v0.7.1

--- nelly/version.py
+++ nelly/version.py
@@ -4,4 +4,4 @@ __email__     = 'moertle@gmail.com'
 __copyright__ = '(c)2008-2020, Matthew Shaw'
 __credits__   = ['Matthew Shaw']
 __license__   = 'MIT'
-__version__   = '0.7'
+__version__   = '0.7.1'
